e3edc737539b24b3b6386428bd8158bd471f0600
Agustin Mendez <mendezagustin94@gmail.com>
2022-02-22 23:35:24 -0300

    Actualización3

--- frontend/src/componentes/layout/Footer.js
+++ frontend/src/componentes/layout/Footer.js
@@ -3,7 +3,7 @@ import '../../estilos/components/layout/Footer.css'
 import React from 'react';
 const Footer = (props) => {
     return (
-        <footer>Diseñado por Agustín Mendez - &copy; 2021</footer>
+        <footer>Diseñado por Agustín Mendez - &copy; 2022</footer>
     );
 }
 export default Footer;
